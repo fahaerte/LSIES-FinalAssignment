845a73ceaf529103dbf80fbd13337624cdceca67
Fabian Haertel <fabian.haertel@tum.de>
2023-05-10 10:43:05 +0300

    Shifted two functionalities to HelperFunc

--- Helper_funcs.py
+++ Helper_funcs.py
@@ -339,3 +339,23 @@ def import_sensor_data(dir_path):
         idx = idx + 1
 
     return df_data_incomplete
+
+
+def compute_period_indices(df, start, end):
+    if pd.to_datetime(end).hour < pd.to_datetime(pd.to_datetime(start)).hour:
+        return np.where(np.logical_or(
+            (df.index.time > pd.to_datetime(start).time()),
+            (df.index.time <= pd.to_datetime(end).time())))[0]
+    else:
+        return np.where(np.logical_and(df.index.time > pd.to_datetime(start).time(),
+                     df.index.time <= pd.to_datetime(end).time()))[0]
+
+
+def get_sensors_based_on_region(df):
+    d = {'sensor names': df['sensor_name'], 'region': df['region']}
+    sensors = pd.DataFrame(d)
+    sensors_region1 = sensors[sensors['region'] == 1]['sensor names']
+    sensors_region2 = sensors[sensors['region'] == 2]['sensor names']
+    sensors_region3 = sensors[sensors['region'] == 3]['sensor names']
+    sensors_region4 = sensors[sensors['region'] == 4]['sensor names']
+    return sensors_region1, sensors_region2, sensors_region3, sensors_region4
